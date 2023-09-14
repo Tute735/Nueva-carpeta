dcb725d6d032557e38afd2f6c6a746da44b30111
tute sarser <matiassarser735@gmail.com>
2023-09-13 21:16:07 -0300

    Entregable 2

--- productManager.js
+++ productManager.js
@@ -1,76 +1,94 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
-      this.products = [];
-      this.lastId = 0;
-    }
-  
-    addProduct(title, description, price, thumbnail, code, stock) {
-      if (!title || !description || !price || !thumbnail || !code || stock === undefined) {
-        console.error("Todos los campos son obligatorios.");
-        return;
-      }
-  
-      if (this.products.some(product => product.code === code)) {
-        console.error("El código de producto ya existe.");
-        return;
-      }
-  
-      this.lastId++;
-      const product = {
-        id: this.lastId,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-      };
-  
-      this.products.push(product);
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    getProductById(id) {
-      const product = this.products.find(product => product.id === id);
-  
-      if (!product) {
-        throw new Error("Producto no encontrado.");
-      }
-  
-      return product;
+  constructor(path) {
+    this.path = path;
+    this.products = this.loadProducts();
+  }
+
+  loadProducts() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf8');
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
     }
   }
-  
-  // Ejemplo de uso:
-  const productManager = new ProductManager();
-  
-  console.log(productManager.getProducts());
-  
-  productManager.addProduct("Producto 1", "Descripción 1", 19.99, "imagen1.jpg", "P001", 10);
-  productManager.addProduct("Producto 2", "Descripción 2", 29.99, "imagen2.jpg", "P002", 15);
-  
-  console.log(productManager.getProducts());
-  
-  try {
-    productManager.addProduct("Producto 3", "Descripción 3", 39.99, "imagen3.jpg", "P003", 20);
-  } catch (error) {
-    console.error(error.message);
+
+  saveProducts() {
+    const data = JSON.stringify(this.products, null, 2);
+    fs.writeFileSync(this.path, data);
   }
-  
-  try {
-    const product = productManager.getProductById(2);
-    console.log("Producto encontrado:", product);
-  } catch (error) {
-    console.error(error.message);
+
+  generateId() {
+    const maxId = this.products.reduce((max, product) => (product.id > max ? product.id : max), 0);
+    return maxId + 1;
   }
-  
-  try {
-    const noExisteProduct = productManager.getProductById(3);
-    console.log("Producto encontrado:", noExisteProduct);
-  } catch (error) {
-    console.error(error.message);
+
+  addProduct({ title, description, price, thumbnail, code, stock }) {
+    const id = this.generateId();
+    const product = { id, title, description, price, thumbnail, code, stock };
+    this.products.push(product);
+    this.saveProducts();
+    return product;
   }
-  
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(id) {
+    return this.products.find(product => product.id === id);
+  }
+
+  updateProduct(id, updatedProduct) {
+    const index = this.products.findIndex(product => product.id === id);
+    if (index !== -1) {
+      this.products[index] = { ...this.products[index], ...updatedProduct };
+      this.saveProducts();
+      return this.products[index];
+    }
+    return null;
+  }
+
+  deleteProduct(id) {
+    const index = this.products.findIndex(product => product.id === id);
+    if (index !== -1) {
+      const removedProduct = this.products.splice(index, 1)[0];
+      this.saveProducts();
+      return removedProduct;
+    }
+    return null;
+  }
+}
+
+// Ejemplo de uso:
+const productManager = new ProductManager('productos.json');
+
+const product1 = productManager.addProduct({
+  title: 'Producto 1',
+  description: 'Descripción 1',
+  price: 19.99,
+  thumbnail: 'imagen1.jpg',
+  code: 'P001',
+  stock: 10
+});
+
+const product2 = productManager.addProduct({
+  title: 'Producto 2',
+  description: 'Descripción 2',
+  price: 29.99,
+  thumbnail: 'imagen2.jpg',
+  code: 'P002',
+  stock: 15
+});
+
+console.log('Productos:', productManager.getProducts());
+console.log('Producto con ID 1:', productManager.getProductById(1));
+
+const updatedProduct = productManager.updateProduct(1, { price: 24.99 });
+console.log('Producto actualizado:', updatedProduct);
+
+const deletedProduct = productManager.deleteProduct(2);
+console.log('Producto eliminado:', deletedProduct);
+console.log('Productos actualizados:', productManager.getProducts());
